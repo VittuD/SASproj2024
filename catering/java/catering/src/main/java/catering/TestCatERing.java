839e77f78831dd765b93938cfb7a940738c2c4c5
davide.vitturini <davide.vitturini@edu.unito.it>
2024-06-06 00:09:18 +0200

    Assignment Builder Pattern implemented

--- catering/java/catering/src/main/java/catering/TestCatERing.java
+++ catering/java/catering/src/main/java/catering/TestCatERing.java
@@ -7,7 +7,7 @@ import catering.businesslogic.event.ServiceInfo;
 import catering.businesslogic.menu.Menu;
 import catering.businesslogic.menu.Section;
 import catering.businesslogic.recipe.Recipe;
-import javafx.collections.ObservableList;
+import catering.persistence.PersistenceManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- catering/java/catering/src/main/java/catering/businesslogic/assignment/Assignment.java
+++ catering/java/catering/src/main/java/catering/businesslogic/assignment/Assignment.java
@@ -1,5 +1,6 @@
 package catering.businesslogic.assignment;
 
+import java.lang.module.ModuleDescriptor;
 import java.time.Duration;
 import java.util.List;
 import catering.businesslogic.recipe.KitchenDuty;
@@ -14,14 +15,75 @@ public class Assignment {
     private List<Cook> cooks;
     private int quantity;
     private KitchenDuty kitchenDuty;
+    private KitchenTurn kitchenTurn;
 
-    public Assignment(KitchenDuty kD, KitchenTurn kitchenTurn, Duration eT, List<Cook> cooks, String desc, boolean completed, int quantity) {
+    /*public Assignment(KitchenDuty kD, KitchenTurn kitchenTurn, Duration eT, List<Cook> cooks, String desc, boolean completed, int quantity) {
         this.kitchenDuty = kD;
         this.description = desc;
         this.estimatedTime = eT;
         this.completed = completed;
         this.cooks = cooks;
         this.quantity = quantity;
+    }*/
+
+    // Builder pattern to avoid telescoping constructor
+    public static class Builder {
+        private String description;
+        private Duration estimatedTime;
+        private boolean completed;
+        private List<Cook> cooks;
+        private int quantity;
+        private KitchenDuty kitchenDuty;
+        private KitchenTurn kitchenTurn;
+
+        public Builder() {}
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder estimatedTime(Duration estimatedTime) {
+            this.estimatedTime = estimatedTime;
+            return this;
+        }
+
+        public Builder completed(boolean completed) {
+            this.completed = completed;
+            return this;
+        }
+
+        public Builder cooks(List<Cook> cooks) {
+            this.cooks = cooks;
+            return this;
+        }
+
+        public Builder quantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public Builder kitchenDuty(KitchenDuty kitchenDuty) {
+            this.kitchenDuty = kitchenDuty;
+            return this;
+        }
+
+        public Builder kitchenTurn(KitchenTurn kitchenTurn) {
+            this.kitchenTurn = kitchenTurn;
+            return this;
+        }
+
+        public Assignment build() {
+            return new Assignment(this);
+        }
+    }
+
+    private Assignment(Builder builder) {
+        this.description = builder.description;
+        this.estimatedTime = builder.estimatedTime;
+        this.completed = builder.completed;
+        this.cooks = builder.cooks;
+        this.quantity = builder.quantity;
     }
 
     public void edit(Turn turn, List<Cook> cooks, String description, boolean completed, int quantity, Duration estimatedTime) {
